1e0467b092420313893d321076647e8a528d7834
evelyn <etersh810@gmail.com>
2024-09-04 15:27:13 -0400

    Time: 42 ms (45.74%), Space: 13 MB (72.05%) - LeetHub

--- 0001-two-sum/0001-two-sum.cpp
+++ 0001-two-sum/0001-two-sum.cpp
@@ -1,26 +1,26 @@
 class Solution {
 public:
     vector<int> twoSum(vector<int>& nums, int target) {
-        // // 1. brute force (n2)
-        // for (int i = 0; i < nums.size(); i++) {
-        //     for (int j = i + 1; j < nums.size(); j++) {
-        //         if (nums[i] + nums[j] == target) {
-        //             return {i, j};
-        //         }
-        //     }
-        // }
-        // return {};
-
-        // 2. reducing time complexitity
+        // 1. brute force (n2)
         for (int i = 0; i < nums.size(); i++) {
-            int need = target - nums[i];
-            auto found = find(nums.begin(), nums.end(), need);
-            if (found != nums.end()) {
-                int idx = distance(nums.begin(), found);
-                if (idx != i)
-                    return {i, idx};
+            for (int j = i + 1; j < nums.size(); j++) {
+                if (nums[i] + nums[j] == target) {
+                    return {i, j};
+                }
             }
         }
+        return {};
+
+        // 2. reducing time complexitity
+        // for (int i = 0; i < nums.size(); i++) {
+        //     int need = target - nums[i];
+        //     auto found = find(nums.begin(), nums.end(), need);
+        //     if (found != nums.end()) {
+        //         int idx = distance(nums.begin(), found);
+        //         if (idx != i)
+        //             return {i, idx};
+        //     }
+        // }
         return {};
     }
 };
