ca12e9494391d16929c500b2b869ccc368f56801
evelyn <evelyn@palominosys.com>
2025-01-26 15:55:35 -0500

    Time: 15 ms (10.88%), Space: 21.7 MB (70.85%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,12 +1,18 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
-        unordered_map<int, bool> m;
-        for (int i = 0; i < nums.size(); i++) {
-            m[nums[i]] = true;
-        }
-        for (int i = 0; i <= nums.size(); i++) {
-            if (m[i] == false)
+        // unordered_map<int, bool> m;
+        // for (int i = 0; i < nums.size(); i++) {
+        //     m[nums[i]] = true;
+        // }
+        // for (int i = 0; i <= nums.size(); i++) {
+        //     if (m[i] == false)
+        //         return i;
+        // }
+        int n = nums.size();
+        sort(nums.begin(), nums.end());
+        for (int i = 0; i <= n; i++) {
+            if (i == n || nums[i] != i)
                 return i;
         }
         return 0;
