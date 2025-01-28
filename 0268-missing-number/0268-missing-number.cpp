64948bb20da9376cc92402cf67d35d30c2468f37
evelyn <evelyn@palominosys.com>
2025-01-27 21:37:39 -0500

    Time: 0 ms (100%), Space: 21.9 MB (39.39%) - LeetHub

--- 0268-missing-number/0268-missing-number.cpp
+++ 0268-missing-number/0268-missing-number.cpp
@@ -1,6 +1,7 @@
 class Solution {
 public:
     int missingNumber(vector<int>& nums) {
+        // 1. O(n), O(n)
         // unordered_map<int, bool> m;
         // for (int i = 0; i < nums.size(); i++) {
         //     m[nums[i]] = true;
@@ -9,12 +10,22 @@ public:
         //     if (m[i] == false)
         //         return i;
         // }
+
+        // 2. O(n log n), O(1)
+        // int n = nums.size();
+        // sort(nums.begin(), nums.end());
+        // for (int i = 0; i <= n; i++) {
+        //     if (i == n || nums[i] != i)
+        //         return i;
+        // }
+
+        // 3. O(n), o(1)
         int n = nums.size();
-        sort(nums.begin(), nums.end());
-        for (int i = 0; i <= n; i++) {
-            if (i == n || nums[i] != i)
-                return i;
+        int sum = (n * (n + 1)) / 2;
+        int have = 0;
+        for (int i = 0; i < n; i++) {
+            have += nums[i];
         }
-        return 0;
+        return sum - have;
     }
 };
